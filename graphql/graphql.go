b3d049fcc6dbfa4b497fb5f98d35c07e1e49d916
AvaChen <avachen@kkstream.com.tw>
2018-09-26 10:22:59 +0800

    remove redundant lines

--- graphql/graphql.go
+++ graphql/graphql.go
@@ -1,10 +1,7 @@
 package graphql
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/kr/pretty"
 
 	"github.com/avachen2005/voyager/graphql/resolver"
 	"github.com/avachen2005/voyager/graphql/schema"
@@ -14,9 +11,6 @@ import (
 )
 
 func GraphqlHandler() http.Handler {
-
-	fmt.Printf("%# v", pretty.Formatter(schema.String()))
-
 	s := graphql.MustParseSchema(schema.String(), &resolver.Resolver{})
 	return &relay.Handler{Schema: s}
 }
